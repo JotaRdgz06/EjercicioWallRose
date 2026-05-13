599383791e0c33958162d71666b9e0cbdc76fdaf
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-05-13 15:58:58 -0600

    LineaOrden implementado

---
+++ LineaOrden.java
@@ -0,0 +1,31 @@
+package Logica;
+
+public class LineaOrden {
+	private double cantidad;
+	private Producto producto;
+	
+	public LineaOrden(double cantidad, Producto producto) {
+		this.cantidad = cantidad;
+		this.producto = producto;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	
+	public double calcularCosto() {
+		return cantidad * producto.getPrecio();
+	}
+}

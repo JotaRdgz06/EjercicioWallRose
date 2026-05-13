1213d795b6277753a03725321c0d2aaa52c51112
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-05-13 15:31:15 -0600

    Producto implementado

---
+++ Producto.java
@@ -0,0 +1,55 @@
+package Logica;
+
+public class Producto {
+	private int codigo;
+	private String nombre;
+	private double existencias;
+	private String unidad;
+	private double precio;
+	
+	public Producto(int codigo, String nombre, double existencias, String unidad, double precio) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.existencias = existencias;
+		this.unidad = unidad;
+		this.precio = precio;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getExistencias() {
+		return existencias;
+	}
+
+	public void setExistencias(double existencias) {
+		this.existencias = existencias;
+	}
+
+	public String getUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(String unidad) {
+		this.unidad = unidad;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	
+}
